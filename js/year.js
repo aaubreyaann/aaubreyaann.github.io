d227ed2f1ed73ce548f6e071f6b67f92951fa4c6
aaubreyaann <stu18002@byui.edu>
2020-05-09 19:02:51 -0700

    Website Planning Document

---
+++ js/year.js
@@ -0,0 +1,4 @@
+
+let curr = new Date();
+let currYear = curr.getFullYear();
+document.getElementById("year").innerHTML = "&copy" + currYear " | Aubrey Stubbs | Nevada | <a href="https://www.byui.edu/online">BYUI Online Learning</a>";
