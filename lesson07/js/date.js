076344a609ee6f31aa71d20788aab0169a766469
aaubreyaann <stu18002@byui.edu>
2020-06-06 11:49:18 -0700

    Transfer of lesson 7 files

---
+++ lesson07/js/date.js
@@ -0,0 +1,15 @@
+const dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+let currDate = new Date()
+let day = currDate.getDay()
+let date = currDate.getDate()
+let month = currDate.getMonth()
+let year = currDate.getFullYear()
+
+document.getElementById("current").innerHTML = dayArray[day] + ", " + date + " " + monthArray[month] + ", " + year
+
+
+addBanner = document.querySelector(".friday")
+if (day == 5) {
+    addBanner.style.display = "block"
+}
