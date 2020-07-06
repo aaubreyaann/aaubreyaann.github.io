8110e45268c132873748a165506b4f048a89fdfc
aaubreyaann <stu18002@byui.edu>
2020-07-06 12:17:16 -0700

    final site

---
+++ lesson11/preston/js/events.js
@@ -0,0 +1,25 @@
+function toggleEvents() {
+    document.getElementsByClassName("events")[0].classList.toggle("show");
+}
+
+apiURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    const towns = jsObject.towns;
+    for (let i = 0; i < towns.length; i++) {
+        if (towns[i].name == "Preston") {
+            let town = jsObject.towns[i];
+            for (let j = 0; j < town.events.length; j++) {
+                let event = document.createElement("li");
+
+                event.textContent = town.events[j];
+
+                document.querySelector('ul.events').appendChild(event);
+            }
+        }
+    }
+  
+});
